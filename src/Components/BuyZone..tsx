0b366d9f12544bffaf4adecc2e0aa5264ad782c1
Huy Nguyen <ng.q.huy98@gmail.com>
2025-04-15 23:15:28 +0300

    prototype upgrade menu

---
+++ src/Components/BuyZone..tsx
@@ -0,0 +1,9 @@
+const BuyZone: React.FC = () => {
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default BuyZone;
--- src/jotai/atoms.ts
+++ src/jotai/atoms.ts
@@ -5,5 +5,67 @@ export const worldAtom = atom(World.Overworld);
 
 
 const essence: Record<string, number> = { underworld: 0, overworld: 0 }
-
 export const essenceAtom = atom(essence);
+
+type Upgrade = {
+    name: string;
+    price: number;
+    isUnlock: boolean;
+}
+const upgrades: Record<string, Upgrade> = {
+    // Underworld upgrades
+    underworld_tier1: { 
+        name: "underworld_tier1",
+        price: 10,
+        isUnlock: false
+    },
+    underworld_tier2: { 
+        name: "underworld_tier2",
+        price: 100,
+        isUnlock: false
+    },
+    underworld_tier3: {
+        name: "underworld_tier3",
+        price: 500,
+        isUnlock: false,
+    },
+    underworld_tier4: {
+        name: "underworld_tier4",
+        price: 1500,
+        isUnlock: false,
+    },
+    underworld_tier5: {
+        name: "underworld_tier5",
+        price: 3000,
+        isUnlock: false,
+    },
+
+    // Overworld upgrades
+    overworld_tier1: { 
+        name: "underworld_tier1",
+        price: 10,
+        isUnlock: false
+    },
+    overworld_tier2: { 
+        name: "underworld_tier2",
+        price: 100,
+        isUnlock: false
+    },
+    overworld_tier3: {
+        name: "underworld_tier3",
+        price: 500,
+        isUnlock: false,
+    },
+    overworld_tier4: {
+        name: "underworld_tier4",
+        price: 1500,
+        isUnlock: false,
+    },
+    overworld_tier5: {
+        name: "underworld_tier5",
+        price: 3000,
+        isUnlock: false,
+    }
+}
+
+export const upgradeAtom = atom(upgrades);
