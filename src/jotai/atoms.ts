7bb27c8a4a606090af70b35ce48ec1adee1d03da
Huy Nguyen <ng.q.huy98@gmail.com>
2025-04-17 15:14:34 +0300

    building object pool

--- src/jotai/atoms.ts
+++ src/jotai/atoms.ts
@@ -3,6 +3,28 @@ import { World } from "../constance";
 
 export const worldAtom = atom(World.Overworld);
 
-
 const essence: Record<string, number> = { underworld: 0, overworld: 0 }
 export const essenceAtom = atom(essence);
+
+type upgradeObj {
+    ttl: number;
+    ready: boolean;
+}
+const objectPool: Record<string, Record<string, Object>> = {
+    underworld: {
+        tier1: {},
+        tier2: {},
+        tier3: {},
+        tier4: {},
+        tier5: {},
+    },
+    overworld: {
+        tier1: {},
+        tier2: {},
+        tier3: {},
+        tier4: {},
+        tier5: {},
+    }
+};
+
+export const objectPoolAtom = atom(objectPool);
